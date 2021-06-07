ffc00b08193196a7c55cb4599a6d8a0d100b73c1
Bukola Omojowo <bomojowo@gmail.com>
2021-06-07 11:11:36 +0100

    Used the JSDoc to code function

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,15 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * Funtion given a number, returns the negative version of that number
+ * @param {number} n -input number
+ * @returns {number} the negative version of number (n)
+ */
 function asNegative(n) {
-  // ...
+  let negNum = (n * -1)
+  return negNum
 }
+
+console.log(asNegative(53))
+
 
 
